e6bb8a7ffebaeff3c8b1a48660edd43583fca9b1
DevJake <devjake202@gmail.com>
2022-08-27 12:43:27 +0100

    Wrapped everything in a try catch because python exceptions are incredibly stupid

--- util/preprocessing/preprocess.py
+++ util/preprocessing/preprocess.py
@@ -47,77 +47,81 @@ def preprocess(eeg_data_dir='./data/subjects', output_dir='./data/outputs/prepro
 
     for subject, session in sub_sess_pairs:
         print(f'Now preprocessing data for Subject {subject}, session {session}')
-        unique_id = str(uuid.uuid4())  # A unique ID for this run
+        try:
+            unique_id = str(uuid.uuid4())  # A unique ID for this run
 
-        raw = pipeline.load_eeg(subject, session)
+            raw = pipeline.load_eeg(subject, session)
 
-        raw = pipeline.apply_montage(raw, f'{eeg_data_dir}/{montage_file_name}.json')
-        raw = pipeline.remove_DC(raw)
-        raw = pipeline.apply_filter(raw,
-                                    low_freq=hypers['PREPROCESSING.LOW_PASS_FILTER.FREQ'],
-                                    high_freq=hypers['PREPROCESSING.HIGH_PASS_FILTER.FREQ'])
-        ica = pipeline.compute_ICA(raw)
-        ica = pipeline.remove_EOG(raw, ica)
-        # TODO assert that the use_ica hyperparameter is True
-        # ica = pipeline.remove_ECG(raw, ica) # Sometimes works, sometimes does not, seems to be an issue with MNE
-        raw = pipeline.apply_ICA_to_RAW(raw, ica)
-        del ica  # It is no longer needed, so we delete it from memory
+            raw = pipeline.apply_montage(raw, f'{eeg_data_dir}/{montage_file_name}.json')
+            raw = pipeline.remove_DC(raw)
+            raw = pipeline.apply_filter(raw,
+                                        low_freq=hypers['PREPROCESSING.LOW_PASS_FILTER.FREQ'],
+                                        high_freq=hypers['PREPROCESSING.HIGH_PASS_FILTER.FREQ'])
+            ica = pipeline.compute_ICA(raw)
+            ica = pipeline.remove_EOG(raw, ica)
+            # TODO assert that the use_ica hyperparameter is True
+            # ica = pipeline.remove_ECG(raw, ica) # Sometimes works, sometimes does not, seems to be an issue with MNE
+            raw = pipeline.apply_ICA_to_RAW(raw, ica)
+            del ica  # It is no longer needed, so we delete it from memory
 
-        _, _, epochs, _ = pipeline.generate_events(raw)
-        path = f'{eeg_data_dir}/preprocessed/Subject {subject}/Session {session}/{unique_id}'
-        os.makedirs(path, exist_ok=True)
-        raw.save(f'{path}/sub_{subject}_sess_{session}_preprocessed.fif')
-        # TODO doesn't seem to be saving
+            _, _, epochs, _ = pipeline.generate_events(raw)
+            path = f'{eeg_data_dir}/preprocessed/Subject {subject}/Session {session}/{unique_id}'
+            os.makedirs(path, exist_ok=True)
+            raw.save(f'{path}/sub_{subject}_sess_{session}_preprocessed.fif')
+            # TODO doesn't seem to be saving
 
-        with open(f'{path}/sub_{subject}_sess_{session}_hyperparams.fif', 'w') as f:
-            json.dump(hypers, f, sort_keys=True, indent=4)
+            with open(f'{path}/sub_{subject}_sess_{session}_hyperparams.fif', 'w') as f:
+                json.dump(hypers, f, sort_keys=True, indent=4)
 
-        del raw
+            del raw
 
-        A, B, C = ['imagined', 'perceived'], ['guitar', 'penguin', 'flower'], ['text', 'sound', 'pictorial']
-        select_epochs = pipeline.select_specific_epochs(epochs, A, B, C)
+            A, B, C = ['imagined', 'perceived'], ['guitar', 'penguin', 'flower'], ['text', 'sound', 'pictorial']
+            select_epochs = pipeline.select_specific_epochs(epochs, A, B, C)
 
-        cropped_epochs = pipeline.crop_epochs(select_epochs)
-        del select_epochs
+            cropped_epochs = pipeline.crop_epochs(select_epochs)
+            del select_epochs
 
-        print('All preprocessing now complete, saving images!')
+            print('All preprocessing now complete, saving images!')
 
-        pbar_epochs = tqdm(len(cropped_epochs), position=0, desc='Epoch progress')
+            pbar_epochs = tqdm(len(cropped_epochs), position=0, desc='Epoch progress')
 
-        for i, p in enumerate(zip(cropped_epochs, cropped_epochs.event_id)):
-            epoch, name = p
-            images = pipeline.generate_eeg_dataset(
-                epoch.squeeze(),
-                per_channel=hypers['RENDER.DO_PER_CHANNEL'],
-                window_width_seconds=hypers['RENDER.WINDOW_WIDTH'],
-                window_overlap_seconds=hypers['RENDER.WINDOW_OVERLAP']
-            )  # Remove outer dimension as this is just 1, so useless
-            # pbar_channels = tqdm(images.shape[0], position=1, desc='Channel progress')
+            for i, p in enumerate(zip(cropped_epochs, cropped_epochs.event_id)):
+                epoch, name = p
+                images = pipeline.generate_eeg_dataset(
+                    epoch.squeeze(),
+                    per_channel=hypers['RENDER.DO_PER_CHANNEL'],
+                    window_width_seconds=hypers['RENDER.WINDOW_WIDTH'],
+                    window_overlap_seconds=hypers['RENDER.WINDOW_OVERLAP']
+                )  # Remove outer dimension as this is just 1, so useless
+                # pbar_channels = tqdm(images.shape[0], position=1, desc='Channel progress')
 
-            for c, channel in enumerate(images):
-                dir = f'{output_dir}/subject_{subject}/session_{session}/channel_{c}'
-                os.makedirs(dir, exist_ok=True)
-                # pbar_event = tqdm(channel.shape[0], position=2, desc='Event progress')
-                for e, event in enumerate(channel):
-                    im = Image.fromarray(event, 'L')
-                    im.save(f'{dir}/epoch_{i}_channel_{c}_event_{e}_{name}.jpg')
+                for c, channel in enumerate(images):
+                    dir = f'{output_dir}/subject_{subject}/session_{session}/channel_{c}'
+                    os.makedirs(dir, exist_ok=True)
+                    # pbar_event = tqdm(channel.shape[0], position=2, desc='Event progress')
+                    for e, event in enumerate(channel):
+                        im = Image.fromarray(event, 'L')
+                        im.save(f'{dir}/epoch_{i}_channel_{c}_event_{e}_{name}.jpg')
 
-                    # pbar_event.update(1)
+                        # pbar_event.update(1)
 
-                # pbar_channels.update(1)
-                # pbar_event.close()
+                    # pbar_channels.update(1)
+                    # pbar_event.close()
 
-            pbar_epochs.update(1)
-            # pbar_channels.close()
+                pbar_epochs.update(1)
+                # pbar_channels.close()
 
-            print(f'Finished saving images for epoch {i}. Name={name}')
+                print(f'Finished saving images for epoch {i}. Name={name}')
 
-        # pbar_epochs.close()
+            # pbar_epochs.close()
 
-        pbar_subjects.update(1)
-        print(f'Completed preprocessing for subject {subject}, session {session}')
+            pbar_subjects.update(1)
+            print(f'Completed preprocessing for subject {subject}, session {session}')
 
-        del cropped_epochs
-
+            del cropped_epochs
+        except Exception as e:
+            print(f'Encountered an exception for Subject {subject} session {session}. Stack trace is as follows...')
+            print(e)
+            continue
 
 preprocess()
