acc350222ab54b4386a9bb43d251b543625f91c9
DevJake <devjake202@gmail.com>
2022-08-28 16:44:19 +0100

    Continued refactoring

--- denoising_diffusion_pytorch/diffusion_models/denoising_diffusion_pytorch.py
+++ denoising_diffusion_pytorch/diffusion_models/denoising_diffusion_pytorch.py
@@ -15,33 +15,8 @@ ModelPrediction = namedtuple('ModelPrediction', ['pred_noise', 'pred_x_start'])
 
 
 # TODO add documentation/descriptions to every method and class
-# TODO backup to git
-
-def extract(a, t, x_shape):
-    b, *_ = t.shape
-    out = a.gather(-1, t)
-    return out.reshape(b, *((1,) * (len(x_shape) - 1)))
-
-
-def linear_beta_schedule(timesteps):
-    scale = 1000 / timesteps
-    beta_start = scale * 0.0001
-    beta_end = scale * 0.02
-    return torch.linspace(beta_start, beta_end, timesteps, dtype=torch.float64)
-
-
-def cosine_beta_schedule(timesteps, s=0.008):
-    """
-    # TODO add comment on what parameter 's' does/is
-    Cosine beta schedule
-    as proposed in https://openreview.net/forum?id=-NEXDKk8gZ
-    """
-    steps = timesteps + 1
-    x = torch.linspace(0, timesteps, steps, dtype=torch.float64)
-    alphas_cumprod = torch.cos(((x / timesteps) + s) / (1 + s) * math.pi * 0.5) ** 2
-    alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
-    betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
-    return torch.clip(betas, 0, 0.999)
+# TODO add WandB.ai support
+# TODO generate model diagram and per-layer parameter count
 
 
 class GaussianDiffusion(nn.Module):
