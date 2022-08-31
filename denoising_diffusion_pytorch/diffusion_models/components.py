5fa21bfdbce304b6da53917a0e4f6b7671c89f18
DevJake <devjake202@gmail.com>
2022-08-31 12:01:50 +0100

    More device calls

--- denoising_diffusion_pytorch/diffusion_models/components.py
+++ denoising_diffusion_pytorch/diffusion_models/components.py
@@ -64,11 +64,10 @@ class SinusoidalPositionalEmbedding(nn.Module):
         self.dim = dim
 
     def forward(self, x):
-        # device = x.device
+        device = x.device
         half_dim = self.dim // 2
         emb = math.log(10000) / (half_dim - 1)
-        # emb = torch.exp(torch.arange(half_dim, device=device) * -emb)
-        emb = torch.exp(torch.arange(half_dim) * -emb)
+        emb = torch.exp(torch.arange(half_dim, device=device) * -emb)
         emb = x[:, None] * emb[None, :]
         emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
         return emb
